512c6ff1548c0e10b8d1e9981b29d74523343531
Theo Boateng <Theoboateng2002@gmail.com>
2025-08-17 16:51:05 -0400

    implemented string interpolation, inferred typing, and ternary operator

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = answer < 9
+                ? $"{answer} is less than nine"
+                : $"{answer} is reater or equal to nine";
+            Console.WriteLine(response);
+
+
+
         }
     }
 }
